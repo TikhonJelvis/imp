74ff73c563bc5fd1d8d21b405af6af5b0bc39fa8
Tikhon Jelvis <tikhon@jelv.is>
2015-12-14 16:17:59 -0800

    Set up a basic skeleton for the project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# IMP Stuff
+
+Some code for analyzing programs in IMP, a tiny language usually used to teach about programming language semantics.
---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "imp";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        description = "Stuff for playing around with IMP programs";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
