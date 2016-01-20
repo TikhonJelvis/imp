be4766df8817dfc1d486f27f924f88b74e198a4b
Tikhon Jelvis <tikhon@jelv.is>
2016-01-19 16:19:55 -0800

    Fixed my compiler. The gcd example works now!

--- src/ImpToZ3.hs
+++ src/ImpToZ3.hs
@@ -18,18 +18,23 @@ type Z3Var = AST
 type Vars = Map Name Z3Var
 
 getZ3Var :: Name -> Vars -> Z3Var
-getZ3Var name scope = fromJust $ Map.lookup name scope
+getZ3Var name scope = case Map.lookup name scope of
+  Just x  -> x
+  Nothing -> error $ "Variable " ++ show name ++ " does not exist!"
+
+makeVar :: Name -> Z3 AST
+makeVar name = Z3.mkFreshBvVar (show name) width
 
 makeVars :: [Name] -> Z3 Vars
 makeVars names = foldM addVar Map.empty names
-  where addVar vars name = do var <- Z3.mkFreshBvVar (show name) width
+  where addVar vars name = do var <- makeVar name
                               return (Map.insert name var vars)
 
 width :: Int
 width = 32
 
 bound :: Int
-bound = 1000
+bound = 10
 
 unroll :: Int -> Cmd -> Cmd
 unroll bound = \case
@@ -59,7 +64,7 @@ bexp :: Vars -> BExp -> Z3 AST
 bexp scope = \case
   True'        -> Z3.mkBool True
   False'       -> Z3.mkBool False
-  e_1 :<=: e_2 -> op Z3.mkLe (aexp scope e_1) (aexp scope e_2)
+  e_1 :<=: e_2 -> op Z3.mkBvsle (aexp scope e_1) (aexp scope e_2)
   e_1 :==: e_2 -> op Z3.mkEq (aexp scope e_1) (aexp scope e_2)
   b_1 :|: b_2  -> do b_1 <- bexp scope b_1
                      b_2 <- bexp scope b_2
@@ -74,7 +79,7 @@ cmd inputs = compile inputs . unroll bound
   where compile scope = \case
           Skip            -> return scope
           Set name val    -> do newVal <- aexp scope val
-                                newVar <- Z3.mkFreshBvVar (show name) width
+                                newVar <- makeVar name
                                 constraint <- Z3.mkEq newVar newVal
                                 Z3.assert constraint
                                 return $ Map.insert name newVar scope
@@ -113,12 +118,13 @@ opts = Z3.opt "MODEL" True
 --  y_3 = y_ 1 + 11
 --  z_1 = y_3
 --  x_2 = ite(cond, x_1, x_0)
---  y_4 = ite(cond,y_2, y_3)
+--  y_4 = ite(cond, y_2, y_3)
 --  z_2 = ite(cond, z_0, z_1)
 makePhis :: AST -> Vars -> Vars -> Vars -> Z3 Vars
 makePhis cond original scope' scope'' = foldM go original $ Map.keys original
   where go scope name = do
-          newVar <- Z3.mkFreshBvVar (show name) width
-          constraint <- Z3.mkIte cond (getZ3Var name scope') (getZ3Var name scope'')
+          newVar     <- makeVar name
+          ite        <- Z3.mkIte cond (getZ3Var name scope') (getZ3Var name scope'')
+          constraint <- Z3.mkEq newVar ite
           Z3.assert constraint
           return $ Map.insert name newVar scope
